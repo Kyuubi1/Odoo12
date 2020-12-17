d7890ac05c98c6bf9d4add3a7e6268ad2f7b39a8
ducanh <anhnguyen1@tamdongtam.vn>
2020-12-17 10:43:28 +0700

    Remove aws

--- addons_custom/const.py
+++ addons_custom/const.py
@@ -1,5 +1,1 @@
-BUCKET_NAME = 'lose-weight-production'
-ENDPOINT_S3 = 'https://lose-weight-production.s3-ap-southeast-1.amazonaws.com/'
-AWS_ACCESS_KEY_ID = 'AKIAJZFJ2MWPMNRFDSIA'
-AWS_SECRET_ACCESS_KEY = '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'
 
