04594db0e4b80e5de661c97329eafbf05da58800
ducanh <anhnguyen1@tamdongtam.vn>
2020-12-29 18:54:52 +0700

    Add_required

--- addons_custom/lw_user/models/res_partner.py
+++ addons_custom/lw_user/models/res_partner.py
@@ -6,15 +6,15 @@ from odoo import models, fields, api
 class ResPartnerUer(models.Model):
     _inherit = 'res.partner'
 
-    x_lw_password = fields.Char(string="Password", required=1, default='123')
-    x_lw_height = fields.Float(string="Height", required=1, default=0)
-    x_lw_weight = fields.Float(string="Weight", required=1, default=0)
-    x_lw_bmi = fields.Float(string="BMI", required=1, compute="_compute_bmi")
+    x_lw_password = fields.Char(string="Password", default='123')
+    x_lw_height = fields.Float(string="Height", default=0)
+    x_lw_weight = fields.Float(string="Weight", default=0)
+    x_lw_bmi = fields.Float(string="BMI", compute="_compute_bmi")
     x_lw_dob = fields.Datetime(string="BirthDay")
     x_lw_gender = fields.Selection([
         ('male', 'Male'),
         ('female', 'Female')
-    ], string="Gender", required=1, default='female')
+    ], string="Gender", default='female')
     x_lw_expected_weight = fields.Float(string="Expected Weight")
     x_lw_steps_per_day = fields.Float(string="Steps Per Day")
     x_lw_meals_per_day = fields.Float(string="Meals Per Day", default=3)
@@ -24,7 +24,7 @@ class ResPartnerUer(models.Model):
         ('normal', 'Normal'),
         ('strong', 'Strong'),
         ('very_strong', 'Very Strong')
-    ], string="Power", required=1, default='normal')
+    ], string="Power", default='normal')
 
     facebook_user_id = fields.Char()
     ocn_token = fields.Char()
